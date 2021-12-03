0680784d77683c82e4e32f280fc947786ba719a7
nelson pineda <pineda_puente@hotmail.com>
2021-12-03 02:18:28 -0300

    agregamos el index

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// el elemento id texto saludo sera cambiado
+// const texto_saludo= document.querySelector('#textoSaludo');
+// texto_saludo.innerHTML = 'hola este parrafo fue modificado';
+
+// const entrada_uno = document.querySelector('#entradaUno');
+// entrada_uno.setAttribute('value', 'Clic Aqui');
+// entrada_uno.setAttribute('id', 'clicUno');
+// entrada_uno.setAttribute('type', 'button');
+
+// entrada_uno.style.color = 'red';
+// //entrada_uno.style.background-color = 'green';
+// entrada_uno.setAttribute('class', 'fondo-verde');
+
+//ejercicio pagina 11
+const entrada = document.querySelector('.lista');
+entrada.innerHTML ='ingrese el nombre:';
+const entrada2=Document.querySelector('#lista2');
+entrada2.setAttribute('type','submit');
+
